1c7594b60d5408ad4a93698e990d9bedce0342a6
sib4321 <sib4321@ukr.net>
2018-09-20 09:12:52 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <SearchBar @termChange="onTermChange"></SearchBar>
+        <div class="row">
+            <VideoDetail :video="selectedVideo" />
+            <VideoList  @videoSelect="onVideoSelect" :videos="videos"></VideoList>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
+
+const API_KEY = 'AIzaSyAXWuRmdPAXHEljlh_WnVzdENoMol-x9xA';
+
+    export default {
+        name: 'App',
+        components: {
+            SearchBar,
+            VideoList,
+            VideoDetail
+        },
+        data() {
+            return { videos: [], selectedVideo: null };
+        },
+        methods: {
+            onVideoSelect(video) {
+                this.selectedVideo = video;
+            },
+            onTermChange(searchTerm) {
+                axios.get('https://www.googleapis.com/youtube/v3/search', {
+                    params: {
+                        key: API_KEY,
+                        type: 'video',
+                        part: 'snippet',
+                        q: searchTerm 
+                    }
+                })
+                .then(response => {
+                    this.videos = response.data.items
+                });
+            }
+        }     
+    };
+</script>
